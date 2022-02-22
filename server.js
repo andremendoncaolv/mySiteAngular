80494ccfa29048cff9857fb28dcc706c12f309f5
Andre <andremendoncaolv@gmail.com>
2022-02-22 17:07:12 -0300

    Alteração do servidor para rodar no Heroku

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 //Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/mySite'));
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + './dist/my-site'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/app-heroku/'}),
+    res.sendFile(__dirname + '/dist/my-site/index.html'),
 );
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(PORT, () => {
+    console.log('Servidor iniciado na porta ' + PORT);
+});
